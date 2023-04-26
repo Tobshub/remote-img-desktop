764edb25e26b4ca21277206eec4df4b2e9ad07a9
Tobshub <tobanigeorge@gmail.com>
2023-04-26 06:17:03 +0100

    create tauri app

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+const root = document.getElementById("root") as HTMLElement;
+if (!root.innerHTML) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
