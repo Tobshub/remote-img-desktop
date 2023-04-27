5deff3560a0c6b759f32d7c1b8dda738a591fb92
Tobshub <tobanigeorge@gmail.com>
2023-04-27 07:22:41 +0100

    added eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), viteTsConfigPaths()],
+  plugins: [react(), viteTsConfigPaths(), eslint()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
